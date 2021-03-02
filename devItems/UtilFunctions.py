695d5998c9f670984970f4f96736612154442dae
hungphd <hungphd@iastate.edu>
2021-03-02 03:34:29 -0600

    update

---
+++ devItems/UtilFunctions.py
@@ -0,0 +1,51 @@
+import os
+import spacy
+from spacy.lang.en import English
+import networkx as nx
+import matplotlib.pyplot as plt
+from nltk.tokenize import word_tokenize
+from numpy import unique
+
+
+def createDirIfNotExist(fopOutput):
+    try:
+        # Create target Directory
+        os.makedirs(fopOutput, exist_ok=True)
+        #print("Directory ", fopOutput, " Created ")
+    except FileExistsError:
+        print("Directory ", fopOutput, " already exists")
+
+
+def initDefaultTextEnvi():
+    nlp_model = spacy.load('en_core_web_sm')
+    nlp = English()
+    nlp.add_pipe(nlp.create_pipe('sentencizer'))
+    return nlp_model,nlp
+
+def getSentences(text,nlp):
+    result=None
+    try:
+        document = nlp(text)
+        result= [sent.string.strip() for sent in document.sents]
+    except Exception as e:
+        print('sone error occured {}'.format(str(e)))
+    return result
+
+
+def preprocess(textInLine):
+    text = textInLine.lower()
+    doc = word_tokenize(text)
+    # doc = [word for word in doc if word in words]
+    # doc = [word for word in doc if word.isalpha()]
+    return ' '.join(doc)
+
+
+
+# Python program to illustrate the intersection
+# of two lists in most simple way
+def intersection(lst1, lst2):
+    lst3 = [value for value in lst1 if value in lst2]
+    return lst3
+def diff(li1, li2):
+    li_dif = [i for i in li1 + li2 if i not in li1 or i not in li2]
+    return li_dif
