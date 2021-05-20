e12fff781018a8e316fb097e2488fa4ff4d52b2a
hungphd <hungphd@iastate.edu>
2021-05-19 23:52:09 -0500

    update

--- devItems/spocExtraction/GenerateGraphFromText.py
+++ devItems/spocExtraction/GenerateGraphFromText.py
@@ -1,7 +1,104 @@
 from nltk.parse import stanford
+from nltk.tree import Tree
+import json
 import os
-os.environ['STANFORD_PARSER'] = '/Users/hungphan/git/dataPapers/StanfordParser/stanford-parser-full-2020-11-17'
-os.environ['STANFORD_MODELS'] = '/Users/hungphan/git/dataPapers/StanfordParser/stanford-parser-full-2020-11-17'
-sp = stanford.StanfordParser()
-trees = [tree for tree in sp.parse("this is a sentence".split())]
-print(trees[0])
+import sys
+# os.environ['STANFORD_PARSER'] = '../../../dataPapers/StanfordParser/stanford-parser-full-2020-11-17'
+# os.environ['STANFORD_MODELS'] = '../../../dataPapers/StanfordParser/stanford-parser-full-2020-11-17'
+
+fopStanfordCoreNLP='../../../dataPapers/stanford-corenlp-4.2.2/'
+
+# sp = stanford.StanfordParser()
+#
+# # def tree_to_dict(tree):
+# #     tdict = {}
+# #     for t in tree:
+# #         if isinstance(t, Tree) and isinstance(t[0], Tree):
+# #             tdict[t.node] = tree_to_dict(t)
+# #         elif isinstance(t, Tree):
+# #             tdict[t.node] = t[0]
+# #     return tdict
+# #
+# # def dict_to_json(dict):
+# #     return json.dumps(dict)
+#
+# def getTree(treeObj,index,lstTreeInfos):
+#     lstIndent=[]
+#     for i in range(0,index):
+#         lstIndent.append('\t')
+#     print(treeObj)
+#     lstIndent.append(treeObj)
+#     strInfo=''.format(lstIndent)
+#     lstTreeInfos.append(strInfo)
+#     for item in treeObj.leaves():
+#         indexItem=index+1
+#         getTree(item,indexItem,lstTreeInfos)
+#
+#
+#
+# trees = [tree for tree in sp.parse("this is a sentence . \n Hello how are you?".split())]
+# tree=trees[0]
+# # output_json = dict_to_json({tree.node: tree_to_dict(tree)})
+#
+# # print('{}\n{}\naaa'.format(type(trees[0]),trees[0]))
+# # print('json: {}'.format(output_json))
+# lstTreeInfos=[]
+# index=0
+# # print(tree.pretty_print())
+# getTree(trees[0],index,lstTreeInfos)
+# strInfo='\n'.join(lstTreeInfos)
+# print(strInfo)
+
+def tree2dict(tree):
+  return {tree.label(): [tree2dict(t) if isinstance(t, Tree) else t
+                      for t in tree]}
+
+def textToJson(strText):
+  try:
+    output = nlp.annotate(text, properties={
+      'annotators': 'parse',
+      'outputFormat': 'json'
+    })
+    jsonTemp = json.loads(output)
+    strJsonObj = json.dumps(jsonTemp, indent=1)
+    # print(type(jsonObject))
+    # strTree = jsonTemp['sentences'][0]['parse']
+    # tree2 = Tree.fromstring(strTree)
+    # dictTree =tree2dict(tree2)
+    # strJsonObj=json.dumps(dictTree,indent=1)
+    # jsonObj=json.loads(strJsonObj)
+  except:
+    strJsonObj='Error'
+  return strJsonObj
+
+def printTree(jsonObj):
+
+  if isinstance(jsonObj,list):
+    for item in jsonObj:
+      # print('go here')
+      printTree(item)
+  elif isinstance(jsonObj,str):
+    print(jsonObj)
+  else:
+    keys = jsonObj.keys()
+    if (len(keys)>0):
+      for key in keys:
+        print(key)
+        printTree(jsonObj[key])
+
+
+from pycorenlp import StanfordCoreNLP
+nlp = StanfordCoreNLP('http://localhost:9000')
+
+text = "The old oak tree from India fell down. I love you."
+
+
+
+jsonObject=textToJson(text)
+print('{}'.format(jsonObject))
+# printTree(jsonObject)
+
+# jsonObj=json.dumps(jsonObject,indent=1)
+# print(jsonObj)
+
+# print('{}'.format(jsonObject['sentences'][0]['parse'])) # tagged output sentence
---
+++ devItems/spocExtraction/GenerateParsedTreeFromPseudoCode.py
@@ -0,0 +1,85 @@
+import glob
+import sys, os
+import operator
+import clang.cindex
+from nltk.parse import stanford
+from nltk.tree import Tree
+import json
+import os
+import sys
+from pycorenlp import StanfordCoreNLP
+nlp = StanfordCoreNLP('http://localhost:9000')
+
+
+sys.path.append(os.path.abspath(os.path.join('..')))
+from UtilFunctions import createDirIfNotExist,getPOSInfo,writeDictToFileText,runASTGenAndSeeResult
+
+def textToJson(strText):
+  try:
+    output = nlp.annotate(strText, properties={
+      'annotators': 'parse',
+      'outputFormat': 'json'
+    })
+    jsonTemp = json.loads(output)
+    strJsonObj = json.dumps(jsonTemp, indent=1)
+    # strJsonObj =str(jsonTemp)
+  except:
+    strJsonObj='Error'
+  return strJsonObj
+
+
+
+
+def genGraph(fpPseudoCode,fopGraph):
+    createDirIfNotExist(fopGraph)
+    f1=open(fpPseudoCode)
+    strPseudoCodes=f1.read()
+    f1.close()
+    dictPseudoCodes={}
+    arrPseudoCodes=strPseudoCodes.split('\n')
+    for i in range(0,len(arrPseudoCodes)):
+        strTrim=arrPseudoCodes[i].strip()
+        if strTrim.endswith('_text.txt'):
+            currentKey=strTrim
+            lstPseudoCodes=[]
+            dictPseudoCodes[currentKey]=lstPseudoCodes
+            # print(currentKey)
+        else:
+            dictPseudoCodes[currentKey].append(arrPseudoCodes[i])
+    index=0
+    for key in dictPseudoCodes.keys():
+        index=index+1
+        pseudoCodeName=key.replace('_text.txt','')
+        lstPseudoCodes=dictPseudoCodes[key]
+        strPseudo='\n'.join(lstPseudoCodes).strip().replace('.',' PUNC_CHAR ')
+        strPseudo =strPseudo.replace('\n',' . ')
+        # lstPseudoCodes='\n'.join(lstPseudoCodes).strip().split('\n')
+        fpItemPseudoCode=fopGraph+pseudoCodeName+'_graphNL.txt'
+        # createDirIfNotExist(fopItemPseudoCode)
+        print('{}\t{}'.format(index,fpItemPseudoCode))
+        strJson = textToJson(strPseudo)
+        f1 = open(fpItemPseudoCode, 'w')
+        f1.write(strJson)
+        f1.close()
+        # for i in range(0,len(lstPseudoCodes)):
+        #     strI=str(i+1)+'.txt'
+        #     strJson=textToJson(lstPseudoCodes[i].strip())
+        #     fpI=fopItemPseudoCode+strI
+        #
+        #     f1=open(fpI,'w')
+        #     f1.write(strJson)
+        #     f1.close()
+fopData='../../../dataPapers/'
+fopTextInSPoC=fopData+'textInSPOC/'
+fpPseudoCodeTestP= fopTextInSPoC + 'combinePseudoCode_TestP.txt'
+fopNLGraphTestP=fopTextInSPoC+'nlGraphInSPOC_TestP/'
+fpPseudoCodeTestW= fopTextInSPoC + 'combinePseudoCode_TestW.txt'
+fopNLGraphTestW=fopTextInSPoC+'nlGraphInSPOC_TestW/'
+fpPseudoCode= fopTextInSPoC + 'combinePseudoCode.txt'
+fopNLGraph=fopTextInSPoC+'nlGraphInSPOC/'
+
+genGraph(fpPseudoCodeTestP,fopNLGraphTestP)
+genGraph(fpPseudoCodeTestW,fopNLGraphTestW)
+genGraph(fpPseudoCode,fopNLGraph)
+
+print('finish')
