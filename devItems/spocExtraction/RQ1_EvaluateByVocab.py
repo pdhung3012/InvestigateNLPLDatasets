91c1e696a49c8a4d6c1b0ea100cb5bfe3f7a05d7
hungphd <hungphd@iastate.edu>
2021-06-08 00:33:57 -0500

    update

--- devItems/spocExtraction/RQ1_EvaluateByVocab.py
+++ devItems/spocExtraction/RQ1_EvaluateByVocab.py
@@ -10,19 +10,11 @@ sys.path.append(os.path.abspath(os.path.join('..')))
 from UtilFunctions import createDirIfNotExist,getPOSInfo,writeDictToFileText,runASTGenAndSeeResult,getGraphDependencyFromText
 import re
 
-fopData='../../../dataPapers/textInSPOC/'
-fpPSPreprocessTrain=fopData+'ps_preprocess_Train.txt'
-fpPSPreprocessTestP=fopData+'ps_preprocess_TestP.txt'
-fpPSPreprocessTestW=fopData+'ps_preprocess_TestW.txt'
-fopVarInfo=fopData+'varInfo/'
-createDirIfNotExist(fopVarInfo)
-fpVarInfoTrain=fopVarInfo+ 'varInfo_Train.txt'
-fpVarInfoTestP=fopVarInfo+ 'varInfo_TestP.txt'
-fpVarInfoTestW=fopVarInfo+ 'varInfo_TestW.txt'
-fpDictLiterals=fopData+'ps_preprocess_dictLiterals.txt'
 distanceInRange=33
 
-def evaluateDetectVarByVocabulary(fpPreprocessTest,fpVarInfoTrain,fpVarInfoTest,fpDictLiterals):
+
+
+def evaluateDetectVarByVocabulary(fpPSPreprocessTestP,fpPSPreprocessTestW,fpVarInfoTrain,fpVarInfoTestP,fpVarInfoTestW,fpResultTestP,fpResultTestW,fpResultDetailsTotal,fpDictLiterals):
     f1 = open(fpPSPreprocessTestP, 'r')
     strPseudoCodes = f1.read()
     arrPseudoCodes = strPseudoCodes.strip().split('\n')
@@ -65,23 +57,29 @@ def evaluateDetectVarByVocabulary(fpPreprocessTest,fpVarInfoTrain,fpVarInfoTest,
     f1.close()
     currentKey = ''
     dictVarsInTraining={}
+    setOfVarsInTraining=[]
     for i in range(0, len(arrVarInfo)):
         strStripItem = arrVarInfo[i].strip()
         # print(strStripItem)
-        if strStripItem.endswith('_text.txt'):
-            strKey = strStripItem.replace('_text.txt', '')
+        if strStripItem.endswith('_code.cpp'):
+            strKey = strStripItem.replace('_code.cpp', '')
             dictLineAndVar={}
             dictVarsInTraining[strKey] = dictLineAndVar
             currentKey = strKey
+            # print('go here')
         elif currentKey in dictVarsInTraining.keys():
             dictLineAndVar = dictVarsInTraining[currentKey]
             arrStripItem=strStripItem.split('\t')
             if (len(arrStripItem)>=3):
+                # print('go here')
                 if not arrStripItem[1].endswith('Literal'):
                     if not arrStripItem[0] in dictLineAndVar.keys():
                         dictLineAndVar[arrStripItem[0]]=[]
                     else:
+                        setOfVarsInTraining.append(arrStripItem[2])
                         dictLineAndVar[arrStripItem[0]].append(arrStripItem[2])
+    setOfVarsInTraining=set(setOfVarsInTraining)
+    print('train var info {} {}'.format(len(dictVarsInTraining.keys()),len(setOfVarsInTraining)))
 
     f1 = open(fpVarInfoTestP, 'r')
     strVarInfo = f1.read()
@@ -92,8 +90,8 @@ def evaluateDetectVarByVocabulary(fpPreprocessTest,fpVarInfoTrain,fpVarInfoTest,
     for i in range(0, len(arrVarInfo)):
         strStripItem = arrVarInfo[i].strip()
         # print(strStripItem)
-        if strStripItem.endswith('_text.txt'):
-            strKey = strStripItem.replace('_text.txt', '')
+        if strStripItem.endswith('_code.cpp'):
+            strKey = strStripItem.replace('_code.cpp', '')
             dictLineAndVar={}
             dictVarsInTestP[strKey] = dictLineAndVar
             currentKey = strKey
@@ -116,8 +114,8 @@ def evaluateDetectVarByVocabulary(fpPreprocessTest,fpVarInfoTrain,fpVarInfoTest,
     for i in range(0, len(arrVarInfo)):
         strStripItem = arrVarInfo[i].strip()
         # print(strStripItem)
-        if strStripItem.endswith('_text.txt'):
-            strKey = strStripItem.replace('_text.txt', '')
+        if strStripItem.endswith('_code.cpp'):
+            strKey = strStripItem.replace('_code.cpp', '')
             dictLineAndVar={}
             dictVarsInTestW[strKey] = dictLineAndVar
             currentKey = strKey
@@ -132,8 +130,233 @@ def evaluateDetectVarByVocabulary(fpPreprocessTest,fpVarInfoTrain,fpVarInfoTest,
                         dictLineAndVar[arrStripItem[0]].append(arrStripItem[2])
 
 
+    f1 = open(fpResultDetailsTotal, 'w')
+    f1.write('')
+    f1.close()
 
 #    evaluate TestP
+    lstKeysTest=dictPseudoCodesTestP.keys()
+    dictResultForTest={}
+    for key in lstKeysTest:
+        arrPseudoCodes=dictPseudoCodesTestP[key]
+        if not key in dictVarsInTestP.keys():
+            continue
+        dictVarInfoItems=dictVarsInTestP[key]
+        lstResultItem=[]
+        print(key)
+        for indexPSLine in range(len(arrPseudoCodes)):
+            strPSLine=arrPseudoCodes[indexPSLine]
+            arrPSTokens=strPSLine.split()
+
+#            list of expected varName in code
+            strIndexInRealFile=str((indexPSLine+34))
+
+            if strIndexInRealFile in dictVarInfoItems.keys():
+                setVarInCode=set(dictVarInfoItems[strIndexInRealFile])
+                setExpectedVarInPS=[]
+                setPredictedVarInPS = []
+                numTP = 0
+                numTN = 0
+                numFP = 0
+                numFN = 0
+                listTP=[]
+                listFP = []
+                listTN = []
+                listFN = []
+                for idxToken in range(len(arrPSTokens)):
+                    token = arrPSTokens[idxToken]
+                    if token in setVarInCode and token in setOfVarsInTraining:
+                        numTP = numTP + 1
+                        listTP.append(token)
+                    elif (not token in setVarInCode) and (token in setOfVarsInTraining):
+                        numFP = numFP + 1
+                        listFP.append(token)
+                    elif (token in setVarInCode) and (not token in setOfVarsInTraining):
+                        numFN = numFN + 1
+                        listFN.append(token)
+                    else:
+                        numTN = numTN + 1
+                        listTN.append(token)
+                        # setExpectedVarInPS.append(token)
+                        # countExpectedVars=countExpectedVars+1
+                        # if token in setOfVarsInTraining:
+                        #     numDetectCorrectVars=numDetectCorrectVars+1
+                        #     setPredictedVarInPS.append(token)
+                strTP=str(listTP)
+                strFP = str(listFP)
+                strTN=str(listTN)
+                strFN = str(listFN)
+                prec = 0
+                rec = 0
+                if (numTP + numFP) > 0:
+                    prec = numTP * 1.0 / (numTP + numFP)
+                if (numTP + numFN) > 0:
+                    rec = numTP * 1.0 / (numTP + numFN)
+                strLineItem = '{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}'.format((indexPSLine + 1), numTP, numFP,
+                                                                                  numTN, numFN, prec, rec, strTP, strFP,
+                                                                                  strTN, strFN)
+                lstResultItem.append(strLineItem)
+        dictResultForTest[key]=lstResultItem
+
+
+    numTotalTP = 0
+    numTotalTN = 0
+    numTotalFP = 0
+    numTotalFN = 0
+    f1 = open(fpResultTestP, 'w')
+    f1.write('')
+    f1.close()
+    for key in dictResultForTest.keys():
+        strHead=key+'_text.txt'
+        lstItem=[]
+        lstItem.append(strHead)
+        # print(strHead)
+
+        lstVal=dictResultForTest[key]
+        for val in lstVal:
+            lstItem.append(val)
+            arrVal=val.split('\t')
+            if len(arrVal)>=5:
+                numTotalTP=numTotalTP+int(arrVal[1])
+                numTotalFP=numTotalFP+int(arrVal[2])
+                numTotalTN = numTotalTN + int(arrVal[3])
+                numTotalFN = numTotalFN + int(arrVal[4])
+
+        lstItem.append('\n\n\n')
+        f1 = open(fpResultTestP, 'a')
+        f1.write('\n'.join(lstItem))
+        f1.close()
+
+    prec=numTotalTP*1.0/(numTotalTP+numTotalFP)
+    rec=numTotalTP*1.0/(numTotalTP+numTotalFN)
+    strTestResult='TestP accuracy (prec/rec/TP/FP/TN/FN): {}\t{}\t{}\t{}\t{}\t{}\n'.format(prec,rec,numTotalTN,numTotalFP,numTotalTN,numTotalFN)
+    f1=open(fpResultDetailsTotal,'a')
+    f1.write(strTestResult)
+    f1.close()
+
+#    evaluate TestW
+    lstKeysTest=dictPseudoCodesTestW.keys()
+    dictResultForTest={}
+    for key in lstKeysTest:
+        arrPseudoCodes=dictPseudoCodesTestW[key]
+        if not key in dictVarsInTestW.keys():
+            continue
+        dictVarInfoItems=dictVarsInTestW[key]
+        lstResultItem=[]
+        print(key)
+        for indexPSLine in range(len(arrPseudoCodes)):
+            strPSLine=arrPseudoCodes[indexPSLine]
+            arrPSTokens=strPSLine.split()
+
+#            list of expected varName in code
+            strIndexInRealFile=str((indexPSLine+34))
+
+            if strIndexInRealFile in dictVarInfoItems.keys():
+                setVarInCode=set(dictVarInfoItems[strIndexInRealFile])
+                setExpectedVarInPS=[]
+                setPredictedVarInPS = []
+                numTP = 0
+                numTN = 0
+                numFP = 0
+                numFN = 0
+                listTP=[]
+                listFP = []
+                listTN = []
+                listFN = []
+                for idxToken in range(len(arrPSTokens)):
+                    token = arrPSTokens[idxToken]
+                    if token in setVarInCode and token in setOfVarsInTraining:
+                        numTP = numTP + 1
+                        listTP.append(token)
+                    elif (not token in setVarInCode) and (token in setOfVarsInTraining):
+                        numFP = numFP + 1
+                        listFP.append(token)
+                    elif (token in setVarInCode) and (not token in setOfVarsInTraining):
+                        numFN = numFN + 1
+                        listFN.append(token)
+                    else:
+                        numTN = numTN + 1
+                        listTN.append(token)
+                        # setExpectedVarInPS.append(token)
+                        # countExpectedVars=countExpectedVars+1
+                        # if token in setOfVarsInTraining:
+                        #     numDetectCorrectVars=numDetectCorrectVars+1
+                        #     setPredictedVarInPS.append(token)
+                strTP = str(listTP)
+                strFP = str(listFP)
+                strTN = str(listTN)
+                strFN = str(listFN)
+                prec=0
+                rec=0
+                if (numTP + numFP)>0:
+                    prec = numTP * 1.0 / (numTP + numFP)
+                if (numTP + numFN)>0:
+                    rec = numTP * 1.0 / (numTP + numFN)
+                strLineItem = '{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}'.format((indexPSLine + 1), numTP, numFP, numTN, numFN,prec,rec,strTP,strFP,strTN,strFN)
+                lstResultItem.append(strLineItem)
+        dictResultForTest[key]=lstResultItem
+
+    numTotalTP = 0
+    numTotalTN = 0
+    numTotalFP = 0
+    numTotalFN = 0
+    f1 = open(fpResultTestW, 'w')
+    f1.write('')
+    f1.close()
+    for key in dictResultForTest.keys():
+        strHead = key + '_text.txt'
+        lstItem = []
+        lstItem.append(strHead)
+        # print(strHead)
+
+        lstVal = dictResultForTest[key]
+        for val in lstVal:
+            lstItem.append(val)
+            arrVal = val.split('\t')
+            if len(arrVal) >= 5:
+                numTotalTP = numTotalTP + int(arrVal[1])
+                numTotalFP = numTotalFP + int(arrVal[2])
+                numTotalTN = numTotalTN + int(arrVal[3])
+                numTotalFN = numTotalFN + int(arrVal[4])
+
+        lstItem.append('\n\n\n')
+        f1 = open(fpResultTestW, 'a')
+        f1.write('\n'.join(lstItem))
+        f1.close()
+
+    prec = numTotalTP * 1.0 / (numTotalTP + numTotalFP)
+    rec = numTotalTP * 1.0 / (numTotalTP + numTotalFN)
+    strTestResult = 'TestW accuracy (prec/rec/TP/FP/TN/FN): {}\t{}\t{}\t{}\t{}\t{}\n'.format(prec, rec, numTotalTN,
+                                                                                                 numTotalFP, numTotalTN,
+                                                                                                 numTotalFN)
+    f1 = open(fpResultDetailsTotal, 'a')
+    f1.write(strTestResult)
+    f1.close()
+
+
+fopData='../../../dataPapers/textInSPOC/'
+fpPSPreprocessTrain=fopData+'ps_preprocess_Train.txt'
+fpPSPreprocessTestP=fopData+'ps_preprocess_TestP.txt'
+fpPSPreprocessTestW=fopData+'ps_preprocess_TestW.txt'
+fopVarInfo=fopData+'varInfo/'
+createDirIfNotExist(fopVarInfo)
+fpVarInfoTrain=fopVarInfo+ 'varInfo_Train.txt'
+fpVarInfoTestP=fopVarInfo+ 'varInfo_TestP.txt'
+fpVarInfoTestW=fopVarInfo+ 'varInfo_TestW.txt'
+fpDictLiterals=fopData+'ps_preprocess_dictLiterals.txt'
+fpResultTestP=fopData+'rq1_result_TestP.txt'
+fpResultTestW=fopData+'rq1_result_TestW.txt'
+fpResultDetailsTotal=fopData+'rq1_result_details.txt'
+
+
+evaluateDetectVarByVocabulary(fpPSPreprocessTestP,fpPSPreprocessTestW,fpVarInfoTrain,fpVarInfoTestP,fpVarInfoTestW,fpResultTestP,fpResultTestW,fpResultDetailsTotal,fpDictLiterals)
+#
+
+
+
+
+
+
 
 
 
