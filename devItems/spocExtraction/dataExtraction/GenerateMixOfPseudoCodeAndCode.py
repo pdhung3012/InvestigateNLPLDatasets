98437de21759dc9b557abb871fbe1640cf35aa13
hungphd <hungphd@iastate.edu>
2021-07-23 03:26:27 -0500

    update

--- devItems/spocExtraction/dataExtraction/GenerateMixOfPseudoCodeAndCode.py
+++ devItems/spocExtraction/dataExtraction/GenerateMixOfPseudoCodeAndCode.py
@@ -1,1 +1,64 @@
 import glob
+import sys, os
+import operator
+
+sys.path.append(os.path.abspath(os.path.join('..')))
+from UtilFunctions import createDirIfNotExist,getPOSInfo,writeDictToFileText
+
+
+
+
+def generateMixFiles(fopPseudoCode,fopCodeFile,fopOutputMix):
+    createDirIfNotExist(fopOutputMix)
+    lstFileItemSPoC=glob.glob(fopCodeFile+'*_code.cpp')
+    indexFile=-1
+    for index in range(0,len(lstFileItemSPoC)):
+        fpCodeFile=lstFileItemSPoC[index]
+        indexFile=indexFile+1
+        nameOfSubmission=os.path.basename(fpCodeFile).replace('_code.cpp','')
+        fpPseudoCode=fopPseudoCode+nameOfSubmission+'_text.txt'
+        f1=open(fpCodeFile,'r')
+        arrCodeLines=f1.read().strip().split('\n')
+        f1.close()
+        f1 = open(fpPseudoCode, 'r')
+        arrPseudoLines = f1.read().strip().split('\n')
+        f1.close()
+
+    #     replace pseudocode to code
+        for indPseudoCode in range(0,len(arrPseudoLines)):
+            lstStrCodeCombine=[]
+            for ind in range(0,distanceHeader):
+                lstStrCodeCombine.append(arrCodeLines[ind])
+            for ind in range(distanceHeader,len(arrCodeLines)):
+                indInCode=ind-distanceHeader
+                if indInCode==indPseudoCode:
+                    strLinePseudo='// {}'.format(arrPseudoLines[indPseudoCode])
+                    lstStrCodeCombine.append(strLinePseudo)
+                else:
+                    lstStrCodeCombine.append(arrCodeLines[ind])
+            fpVersion=fopOutputMix+nameOfSubmission+'_v'+str(indPseudoCode+1)+'.cpp'
+            f1=open(fpVersion,'w')
+            f1.write('\n'.join(lstStrCodeCombine))
+            f1.close()
+        print('complete {}/{} {}'.format(index,len(lstFileItemSPoC),fpCodeFile))
+
+fopData='../../../../dataPapers/textInSPOC/'
+fopTrainDataCFiles=fopData+'trainOnlyC/'
+fopTestWDataCFiles=fopData+'testWOnlyC/'
+fopTestPDataCFiles=fopData+'testPOnlyC/'
+fopTrainPseudocode=fopData+'trainSPOCPlain/'
+fopTestPPseudocode=fopData+'testPSPOCPlain/'
+fopTestWPseudocode=fopData+'testWSPOCPlain/'
+
+fopMixFiles=fopData+'mix_step1/'
+fopOutputTrainMixFiles=fopMixFiles+'train/'
+fopOutputTestPMixFiles=fopMixFiles+'testP/'
+fopOutputTestWMixFiles=fopMixFiles+'testW/'
+distanceHeader=33
+generateMixFiles(fopTestPPseudocode,fopTestPDataCFiles,fopOutputTestPMixFiles)
+generateMixFiles(fopTestWPseudocode,fopTestWDataCFiles,fopOutputTestWMixFiles)
+generateMixFiles(fopTrainPseudocode,fopTrainDataCFiles,fopOutputTrainMixFiles)
+
+
+
+
