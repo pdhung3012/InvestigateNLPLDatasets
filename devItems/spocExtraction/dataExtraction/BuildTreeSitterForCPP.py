cea3fa517d4099195d77ac6eb23902e566d22913
pdhung3012 <pdhung3012@gmail.com>
2021-07-25 02:35:59 -0500

    extract compiled code

---
+++ devItems/spocExtraction/dataExtraction/BuildTreeSitterForCPP.py
@@ -0,0 +1,42 @@
+import glob
+import sys, os
+import operator
+
+from tree_sitter import Language, Parser
+sys.path.append(os.path.abspath(os.path.join('..')))
+sys.path.append(os.path.abspath(os.path.join('../../')))
+from UtilFunctions import createDirIfNotExist,getPOSInfo,writeDictToFileText
+
+
+fopData='/home/hungphd/dataPapers/'
+fopGithub='/home/hungphd/git/'
+fopBuildFolder=fopData+'build-tree-sitter/'
+createDirIfNotExist(fopBuildFolder)
+
+fpLanguageSo=fopBuildFolder+'my-languages.so'
+fpTreeSitterC=fopGithub+'tree-sitter-c'
+fpTreeSitterCPP=fopGithub+'tree-sitter-cpp'
+
+# Language.build_library(
+#   # Store the library in the `build` directory
+#   fpLanguageSo,
+#
+#   # Include one or more languages
+#   [
+#     fpTreeSitterC,
+#     fpTreeSitterCPP
+#   ]
+# )
+Language.build_library(
+  # Store the library in the `build` directory
+  fopBuildFolder+'/my-languages.so',
+
+  # Include one or more languages
+  [
+    fopGithub+'/tree-sitter-go',
+    fopGithub+'/tree-sitter-javascript',
+    fopGithub+'/tree-sitter-python',
+    fopGithub+'/tree-sitter-c',
+    fopGithub+'/tree-sitter-cpp'
+  ]
+)
