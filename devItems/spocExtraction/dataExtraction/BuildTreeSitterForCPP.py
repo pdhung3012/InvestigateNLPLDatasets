e11ab444a9b066df5aa7eb6f1c9541a5dbf45818
pdhung3012 <pdhung3012@gmail.com>
2021-07-25 02:38:18 -0500

    extract compiled code

--- devItems/spocExtraction/dataExtraction/BuildTreeSitterForCPP.py
+++ devItems/spocExtraction/dataExtraction/BuildTreeSitterForCPP.py
@@ -8,7 +8,7 @@ sys.path.append(os.path.abspath(os.path.join('../../')))
 from UtilFunctions import createDirIfNotExist,getPOSInfo,writeDictToFileText
 
 
-fopData='/home/hungphd/dataPapers/'
+fopData='/home/hungphd/'
 fopGithub='/home/hungphd/git/'
 fopBuildFolder=fopData+'build-tree-sitter/'
 createDirIfNotExist(fopBuildFolder)
