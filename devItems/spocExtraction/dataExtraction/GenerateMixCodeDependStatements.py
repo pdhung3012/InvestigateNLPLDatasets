272e207e7018ce98ab2ec140f2ba236bb4064d3b
pdhung3012 <pdhung3012@gmail.com>
2021-09-08 03:35:55 -0500

    generate graph

--- devItems/spocExtraction/dataExtraction/GenerateMixCodeDependStatements.py
+++ devItems/spocExtraction/dataExtraction/GenerateMixCodeDependStatements.py
@@ -16,13 +16,15 @@ import ast
 import re
 
 strStmtSplit=' StmtSplit '
+strTabChar=' tabChar '
+strEndLineChar=' endLineChar '
 
-def walkJsonAndGetIndent(jsonObject,dictIndents,dictLinesAndElements,indent):
+def walkJsonAndGetIndent(jsonObject,dictLinesAndElements,indent):
     try:
-        if indent not in dictIndents.keys():
-            dictIndents[indent]=[]
+        # if indent not in dictIndents.keys():
+        #     dictIndents[indent]=[]
         jsonObject['indent']=indent
-        dictIndents[indent].append(jsonObject)
+        # dictIndents[indent].append(jsonObject)
 
         startLine=jsonObject['startLine']
         endLine=jsonObject['endLine']
@@ -37,11 +39,11 @@ def walkJsonAndGetIndent(jsonObject,dictIndents,dictLinesAndElements,indent):
             lstChildren=jsonObject['children']
             indentChild=indent+1
             for i in range(0,len(lstChildren)):
-                walkJsonAndGetIndent(lstChildren[i],dictIndents,indentChild)
+                walkJsonAndGetIndent(lstChildren[i],indentChild)
     except:
         traceback.print_exc()
 
-def findReplaceableStatements(dictIndents,dictLinesAndElements,lstTupFunctionDeclarations):
+def findReplaceableStatements(dictLinesAndElements,lstTupFunctionDeclarations):
     try:
         # find tuples of method declarations
         for i in range(0,len(dictLinesAndElements.keys)):
@@ -51,35 +53,6 @@ def findReplaceableStatements(dictIndents,dictLinesAndElements,lstTupFunctionDec
                 endLine=item['endLine']
                 tup=(startLine,endLine)
                 lstTupFunctionDeclarations.append(tup)
-            lstChildren=dictLinesAndElements[item]['children']
-            dictIndentElements={}
-            for j in range(0,len(lstChildren)):
-                itemEleJ=lstChildren[j]
-                indIndent=itemEleJ['indent']
-                if indIndent not in dictIndentElements.keys():
-                    dictIndentElements[indIndent]=[itemEleJ]
-                else:
-                    dictIndentElements[indIndent].append(itemEleJ)
-            dictLinesAndElements[item]['sortedElementsByIndents']=dictIndentElements
-            # main sstatement is the node that is closest to the root. If there are 2 or more nodes in the same deep, we calculate
-            indentDeepest=sorted(dictLinesAndElements[item]['sortedElementsByIndents'].keys())[0]
-            lstPossibleStatements=dictLinesAndElements[item]['sortedElementsByIndents'][indentDeepest]
-            mainStmt=''
-            if len(lstPossibleStatements)==1:
-                selectItem=lstPossibleStatements[0]
-
-            else:
-                selectItem=lstPossibleStatements[0]
-                for j in range(1,len(lstPossibleStatements)):
-                    candItem=selectItem[j]
-                    if candItem['startLine']<selectItem['startLine'] and candItem['endLine']>selectItem['endLine']:
-                        selectItem=candItem
-                    elif candItem['startLine']==selectItem['startLine'] and candItem['endLine']==selectItem['endLine']:
-                        if (candItem['endOffset']-candItem['startOffset'])>(selectItem['endOffset']-selectItem['startOffset']):
-                            selectItem=candItem
-            mainStmt = '{}-{}-{}-{}-{}'.format(selectItem['type'], selectItem['startLine'], selectItem['startOffset'],
-                                           selectItem['endLine'], selectItem['endOffset'])
-            dictLinesAndElements[item]['mainStmt']=mainStmt
 
 
         for item in dictLinesAndElements.keys():
@@ -87,22 +60,80 @@ def findReplaceableStatements(dictIndents,dictLinesAndElements,lstTupFunctionDec
             startLine=int(arrStartEnd[0])
             endLine=int(arrStartEnd[1])
             isInFD=False
+            selectedTup=None
             for tup in lstTupFunctionDeclarations:
                 if startLine>tup[0] and endLine<tup[1]:
                     isInFD=True
+                    selectedTup=tup
                     break
+
             if not isInFD:
                 dictLinesAndElements.pop(item, None)
+                continue
 
+            dictLinesAndElements[item]['scopeOfMethods']=selectedTup
+            lstChildren = dictLinesAndElements[item]['children']
+            dictIndentElements = {}
+            for j in range(0, len(lstChildren)):
+                itemEleJ = lstChildren[j]
+                indIndent = itemEleJ['indent']
+                if indIndent not in dictIndentElements.keys():
+                    dictIndentElements[indIndent] = [itemEleJ]
+                else:
+                    dictIndentElements[indIndent].append(itemEleJ)
+            dictLinesAndElements[item]['sortedElementsByIndents'] = dictIndentElements
+            # main sstatement is the node that is closest to the root. If there are 2 or more nodes in the same deep, we calculate
+            indentDeepest = sorted(dictLinesAndElements[item]['sortedElementsByIndents'].keys())[0]
+            lstPossibleStatements = dictLinesAndElements[item]['sortedElementsByIndents'][indentDeepest]
+            # mainStmt = ''
+            if len(lstPossibleStatements) == 1:
+                selectItem = lstPossibleStatements[0]
+                selectItem['singleRoot']=True
 
+            else:
+                selectItem = lstPossibleStatements[0]
+                selectItem['singleRoot'] = False
+                for j in range(1, len(lstPossibleStatements)):
+                    candItem = selectItem[j]
+                    if candItem['startLine'] < selectItem['startLine'] and candItem['endLine'] > selectItem['endLine']:
+                        selectItem = candItem
+                    elif candItem['startLine'] == selectItem['startLine'] and candItem['endLine'] == selectItem[
+                        'endLine']:
+                        if (candItem['endOffset'] - candItem['startOffset']) > (
+                                selectItem['endOffset'] - selectItem['startOffset']):
+                            selectItem = candItem
+            # mainStmt = '{}-{}-{}-{}-{}'.format(selectItem['type'], selectItem['startLine'], selectItem['startOffset'],
+            #                                    selectItem['endLine'], selectItem['endOffset'])
+            dictLinesAndElements[item]['mainStmt'] = selectItem
+            selectItem['isReplaceable'] = True
 
+            if (selectItem['startLine']-selectedTup[0]>1) and (selectedTup[1] -selectItem['endLine']>1):
+                dictLinesAndElements.pop(item,None)
 
-    #     find replacable element inside func decl
+        #  select the indent with most isReplaceable items
+        dictIndentAndStatements={}
         for i in range(0,len(dictLinesAndElements.keys)):
-            item=dictLinesAndElements.keys()[i]
+            key=dictLinesAndElements.keys()[i]
+            mainStmt=dictLinesAndElements[key]['mainStmt']
+            indent=mainStmt['indent']
+            if indent not in dictIndentAndStatements.keys():
+                dictIndentAndStatements[indent]=1
+            else:
+                dictIndentAndStatements[indent]=dictIndentAndStatements[indent]+1
+        dictIndentAndStatements = dict(sorted(dictIndentAndStatements.items(), key=operator.itemgetter(1), reverse=True))
+        highestIndent=dictIndentAndStatements.keys()[0]
+        for i in range(0,len(dictLinesAndElements.keys)):
+            key=dictLinesAndElements.keys()[i]
+            mainStmt = dictIndentAndStatements[key]['mainStmt']
+            indent = mainStmt['indent']
+            if indent!=highestIndent:
+                dictLinesAndElements.pop(key,None)
+    except:
+        traceback.print_exc()
 
-
-
+def generateMixVersionsAndLabels(dictLinesAndElements,arrCodes,arrPseudos,fopMixVersion,fonameTrainTest,idCode):
+    try:
+        isOK=False
     except:
         traceback.print_exc()
 
@@ -112,7 +143,8 @@ fopPseudoFile=fopRoot+'step2_pseudo_tokenize/'
 fopTreeSitterFile=fopRoot+'step3_treesitter_tokenize/'
 fopLabels=fopRoot+'step2_pseudo_tokenize/'
 fopTreeSitterFile=fopRoot+'step3_treesitter_tokenize/'
-
+fopMixVersion=fopRoot+'step4_genMixCodePseudocode/'
+createDirIfNotExist(fopMixVersion)
 
 lstFpDictASTs=glob.glob(fopTreeSitterFile+'**/*_code_ast.txt',recursive=True)
 distanceHeader=33
@@ -135,10 +167,13 @@ for i in range(0,len(lstFpDictASTs)):
         f1=open(fpItemAST,'r')
         strASTContent=f1.read().strip().split('\t')[1]
         jsonObject = ast.literal_eval(strASTContent)
-        dictIndents={}
         dictLinesAndElements={}
         indent=1
-        walkJsonAndGetIndent(jsonObject,dictIndents,dictLinesAndElements,indent)
+        lstTupFunctionDeclarations=[]
+        walkJsonAndGetIndent(jsonObject,dictLinesAndElements,indent)
+        findReplaceableStatements(dictLinesAndElements, lstTupFunctionDeclarations)
+        print('dict {}'.format(dictLinesAndElements))
+        generateMixVersionsAndLabels(dictLinesAndElements,arrCodes,arrPseudos,fopMixVersion,fonameItemASTidCode)
 
 
 
